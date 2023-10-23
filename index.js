f65b5deb5384f43014be988f2500238a3c7d3068
Mehedi <tanjildewan10@gmail.com>
2023-10-23 13:39:27 +0600

    update dist and backend

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-const express = require('express');
-const app = express();
-const { MongoClient, ObjectId } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require("cors");
+const app = express()
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.watftgx.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function run() {
   try {
-    
-    // Get the database and collection on which to run the operation
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
     const musicCollection = client.db("pod-music").collection("musics");
     const saveMusicCollection = client.db("pod-music").collection("saved");
     const usersMusicCollection = client.db("pod-music").collection("users");
 
-    app.get('/',(req,res)=>{
-      res.send('listen a song')
-    })
+    
 
-    app.get('/music',async(req,res)=>{
+    app.get('/api/music',async(req,res)=>{
       const music = await musicCollection.find().toArray()
       res.send(music)
     })
 
-    app.get('/saved',async(req,res)=>{
+    app.get('/api/saved',async(req,res)=>{
       const email = req.query.email;
       if(!email){
         res.send([])
@@ -38,36 +48,40 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/saved',async(req,res)=>{
+    app.post('/api/saved',async(req,res)=>{
       const item = req.body;
       const result = await saveMusicCollection.insertOne(item)
       res.send(result)
     })
 
-    app.delete('/saved/:id',async(req,res)=>{
+    app.delete('/api/saved/:id',async(req,res)=>{
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)}
+      const query = {_id: new ObjectId(id)}
       const result = await saveMusicCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.post('/users',async(req,res)=>{
+    app.post('/api/users',async(req,res)=>{
       const user = req.body;
       const result = await usersMusicCollection.insertOne(user)
       res.send(result)
     })
 
-    
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//   })
+
+
+
+// app.get('/',(req,res)=>{
+//   res.send('listen a song')
+// })
 
 
   app.listen(port, () => {
