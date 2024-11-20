a066923e6c7b660f3b63e6bd917783df79359600
Mehedi <tanjildewan10@gmail.com>
2024-11-20 14:36:26 +0600

    update approve user

--- index.js
+++ index.js
@@ -130,6 +130,18 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/approve/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id:new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          role:'approve'
+        }
+      }
+      const result = await usersMusicCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
     app.delete("/api/users/:id",async(req,res)=>{
       const id = req.params.id;
       const filter = {_id:new ObjectId(id)}
