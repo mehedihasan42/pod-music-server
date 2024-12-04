abb5d435e36811202ca20386f6d8baa4c7bb450d
Mehedi <tanjildewan10@gmail.com>
2024-12-04 12:05:21 +0600

    fix update user

--- index.js
+++ index.js
@@ -130,12 +130,12 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/approve/users/:id',async(req,res)=>{
+    app.patch('/api/approveUser/:id',async(req,res)=>{
       const id = req.params.id;
       const filter = {_id:new ObjectId(id)};
       const updateDoc={
         $set:{
-          role2:'approve'
+          role:'approve'
         }
       }
       const result = await usersMusicCollection.updateOne(filter,updateDoc)
